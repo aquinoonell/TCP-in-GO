d3d1139bccea0c95291f4e43d999e5cc8e8180fc
don <onellaquino@icloud.com>
2025-08-01 12:56:14 -0400

    TPC

--- internal/request/request.go
+++ internal/request/request.go
@@ -3,6 +3,7 @@ package request
 import (
 	"fmt"
 	"io"
+	"strings"
 )
 
 type Request struct {
@@ -16,6 +17,22 @@ type RequestLine struct {
 }
 
 var ERROR_BAD_START_LINE = fmt.Errorf("ERROR_BAD_START_LINE")
+var SEPARATOR = "\r\n"
+
+func parseRequestLine(b string) (*Request, string, error) {
+	idx := strings.Index(b, SEPARATOR)
+	if idx == -1 {
+		return nil, b , nil
+	}
+
+	startLine := b[:idx]
+	restOfMsg := b[idx+len(SEPARATOR):]
+
+	parts := strings.Split(startLine, " ")
+
+	return &RequestLine{
+	}, restOfMsg, nil
+}
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 }
