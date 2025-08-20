279075421b8ea7c2ccb4cfb99b6b3d1aa2f741a8
don <onellaquino@icloud.com>
2025-08-20 19:10:48 -0400

    Go

--- internal/request/headers/headers.go
+++ internal/request/headers/headers.go
@@ -1,8 +1,33 @@
 package header
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+const crlf = "\r\n"
+
 type Headers map[string]string
 
 func NewHeaders() Headers {
+	return map[string]string{}
 }
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+	if idx == 0 {
+		return 2, true, nil
+	}
+
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+
+	key := strings.ToLower(string(parts[0]))
+
+	if key != strings.TrimRight(key, " ") {
+		return 0, false, fmt.Errorf("Invalid header name: %s", key)
+	}
 }
